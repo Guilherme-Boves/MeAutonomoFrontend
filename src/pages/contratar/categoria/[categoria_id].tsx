19d695f22188c591809f3157122f0338bae992b8
Guilherme-Boves <ghboves@gmail.com>
2022-09-22 00:13:15 -0300

    Tela de listagem de perfis

--- src/pages/contratar/categoria/[categoria_id].tsx
+++ src/pages/contratar/categoria/[categoria_id].tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 import Image from 'next/image';
 import { ReturnButton } from '../../../components/ui/ReturnButton';
--- src/pages/contratar/categoria/[categoria_id]/perfis/[perfis_id].tsx
+++ src/pages/contratar/categoria/[categoria_id]/perfis/[perfis_id].tsx
@@ -1,19 +1,49 @@
-import { useRouter } from 'next/router';
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react'
+import { ReturnButton } from '../../../../../components/ui/ReturnButton';
 import { setupAPIClient } from '../../../../../services/api';
 import { canSSRAuth } from '../../../../../utils/canSSRAuth'
+import styles from '../../styles.module.css'
 
-export default function Perfis() {
+type ItemProps = {    
+    user:{
+        id: string;
+        nome: string;
+    }
+}
 
-    const router = useRouter();
+interface ListPerfisProps {
+    listPerfis: ItemProps[];
+}
 
-    const id = router.query.perfis_id;
+export default function Perfis({ listPerfis }: ListPerfisProps) {
+
+    const [perfis, setPerfis] = useState(listPerfis || []);
+
     return(
 
-        <div>
-            <h1>
-                Perfis dos profissionais: {id}
-            </h1>
+        <div className={styles.body}>
+            <ReturnButton/>
+            <div className={styles.container}>
+                <div className={styles.carrossel}>
+                    {perfis.map((item) => {
+
+                        const {id, nome} = item.user
+
+                        return(                            
+                            <div key={id}>
+                                <Link href={``}>
+                                    <a className={styles.item}>
+                                        <Image src={``} alt={`Foto do Profissional`} width={250} height={250} />
+                                        <div className={styles.nome}>{nome}</div>
+                                    </a>
+                                </Link>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
@@ -21,13 +51,15 @@ export default function Perfis() {
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
 
-    //const id = ctx.query.perfis_id;
+    const tipoDoServicoId = ctx.query.perfis_id;
 
-    //const api = setupAPIClient(ctx);
+    const api = setupAPIClient(ctx);
 
-    //const response = await api.get('')
+    const response = await api.get(`publicarservico?tipoDoServico_id=${tipoDoServicoId}`)
 
     return{
-        props: {}
+        props: {
+            listPerfis: response.data
+        }
     }
 })
