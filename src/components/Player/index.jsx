831af0242f223a41e808a00ff073fc568f77f4c2
Luiz-Camargo <luiz.henriquecamargo@hotmail.com>
2022-11-28 21:03:15 -0300

    Video trocado

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -3,7 +3,7 @@ import { Player as Video, Youtube} from '@vime/react'
 export default function Player(){
   return(
     <Video controls >
-     <Youtube videoId="1itZYO-jROA"/>
+     <Youtube videoId="zGLNEOG1TzI"/>
    </Video>
   )
 }
